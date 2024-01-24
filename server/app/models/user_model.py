0749783d5db5b1f51003a2b96c19158d129d778f
michael <michaelngige@gmail.com>
2024-01-24 12:19:39 +0300

    hashing password

--- server/app/models/user_model.py
+++ server/app/models/user_model.py
@@ -1,13 +1,22 @@
 from app import db
 from sqlalchemy.orm import validates
 from flask import flash
+from flask_bcrypt import Bcrypt
 
-
+bcrypt = Bcrypt()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    _password = db.Column("password", db.String(100), nullable=False)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, plaintext_password):
+        self._password = bcrypt.generate_password_hash(plaintext_password).decode('utf-8')
 
     @validates('username')
     def validate_username(self, key, username):
@@ -31,9 +40,12 @@ class User(db.Model):
         # Add more password validations as needed
         return password
 
+    def check_password(self, plaintext_password):
+        return bcrypt.check_password_hash(self._password, plaintext_password)
+
     def serialize(self):
         return {
             'id': self.id,
             'username': self.username,
-            'password': self.password
+            'password': self._password
         }
